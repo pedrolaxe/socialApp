5f66d014fb41099b92cf6e6aeee557d415cb54db
Pedro Laxe <pedrolaxe@gmail.com>
2020-10-21 12:01:35 -0300

    Forgot Pass and Reset layout

--- client/src/user/ForgotPassword.js
+++ client/src/user/ForgotPassword.js
@@ -26,40 +26,49 @@ class ForgotPassword extends Component {
         const { message, error } = this.state;
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">Ask for Password Reset</h2>
-
-                <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
-                    {error}
-                </div>
-                <div className="alert alert-danger" style={{ display: message ? "" : "none" }}>
-                    {message}
-                </div>
-                
-                <form>
-                    <div className="form-group mt-5">
-                        <input
-                            type="email"
-                            className="form-control"
-                            placeholder="Your email address"
-                            value={this.state.email}
-                            name="email"
-                            onChange={e =>
-                                this.setState({
-                                    email: e.target.value,
-                                    message: "",
-                                    error: ""
-                                })
-                            }
-                            autoFocus
-                        />
+                <div className="row mb-3">
+                <div className="col-md-8">
+                        <img src="https://i.imgur.com/926vnAO.jpg" className="img-fluid rounded" />
                     </div>
-                    <button
-                        onClick={this.forgotPasswordFunction}
-                        className="btn btn-raised btn-primary"
-                    >
-                        Send Password Rest Link
-                    </button>
-                </form>
+                    <div className="col-md-4">
+
+                        <h2 className="mt-5 mb-5">Ask for Password Reset</h2>
+
+                        <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
+                            {error}
+                        </div>
+                        <div className="alert alert-danger" style={{ display: message ? "" : "none" }}>
+                            {message}
+                        </div>
+                    
+                        <form>
+                            <div className="form-group mt-5">
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    placeholder="Your email address"
+                                    value={this.state.email}
+                                    name="email"
+                                    onChange={e =>
+                                        this.setState({
+                                            email: e.target.value,
+                                            message: "",
+                                            error: ""
+                                        })
+                                    }
+                                    autoFocus
+                                />
+                            </div>
+                            <button
+                                onClick={this.forgotPasswordFunction}
+                                className="btn btn-raised btn-primary"
+                            >
+                                Send Link to Reset
+                            </button>
+                        </form>
+                    </div>
+                    
+                </div>
             </div>
         );
     }
--- client/src/user/ResetPassword.js
+++ client/src/user/ResetPassword.js
@@ -33,39 +33,47 @@ class ResetPassword extends Component {
         const { message, error } = this.state;
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">Reset your Password</h2>
-
-                <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
-                    {error}
-                </div>
-                <div className="alert alert-danger" style={{ display: message ? "" : "none" }}>
-                    {message}
-                </div>
-                <form>
-                    <div className="form-group mt-5">
-                        <input
-                            type="password"
-                            className="form-control"
-                            placeholder="Your new password"
-                            value={this.state.newPassword}
-                            name="newPassword"
-                            onChange={e =>
-                                this.setState({
-                                    newPassword: e.target.value,
-                                    message: "",
-                                    error: ""
-                                })
-                            }
-                            autoFocus
-                        />
+                <div className="row mb-3">
+                    <div className="col-md-8">
+                        <img src="https://i.imgur.com/926vnAO.jpg" className="img-fluid rounded" />
                     </div>
-                    <button
-                        onClick={this.resetPassword}
-                        className="btn btn-raised btn-primary"
-                    >
-                        Reset Password
-                    </button>
-                </form>
+                    <div className="col-md-4">
+                    <h2 className="mt-5 mb-5">Reset your Password</h2>
+
+                    <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
+                        {error}
+                    </div>
+                    <div className="alert alert-danger" style={{ display: message ? "" : "none" }}>
+                        {message}
+                    </div>
+                    <form>
+                        <div className="form-group mt-5">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Your new password"
+                                value={this.state.newPassword}
+                                name="newPassword"
+                                onChange={e =>
+                                    this.setState({
+                                        newPassword: e.target.value,
+                                        message: "",
+                                        error: ""
+                                    })
+                                }
+                                autoFocus
+                            />
+                        </div>
+                        <button
+                            onClick={this.resetPassword}
+                            className="btn btn-raised btn-primary"
+                        >
+                            Reset Password
+                        </button>
+                    </form>
+                    </div>
+                   
+            </div>
             </div>
         );
     }
